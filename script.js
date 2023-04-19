c37985f84d5e6a2a78d6a1ce1ecd58a439e93cf4
JuvitaSaini09 <juvitasaini@gmail.com>
2023-04-19 10:56:32 +0530

    docs: comment added

--- script.js
+++ script.js
@@ -20,5 +20,22 @@ var findMinFlights = function (fuelAtAirportsArr) {
   }
 };
 
-//console.log(findMinFlights([3,0,0,3,1]));
+console.log(findMinFlights([3,0,0,3,1]));
 
+//Algo Explantion
+/*
+1) If 
+     check in if whether the first element is zero or not and if it is 0 then return -1;
+2)Else 
+       a)  initialise variables: fuelAirportsArrLength for length of the array ,flightCount as 0 to track the number of jumps or flights,leftAirport and rightAirport to keep track of the starting and max reachable index in array for current iteration
+
+        b) Iterate while loop till last second airport (rightAirport < fuelAtAirportsArrLength - 1) 
+            - intialise the max varibale which will contain the max value among the possible jumps from current position;
+            - iterate the array from leftAirport to rightAIrport to compute the max value among the possible jumps and save it into the max varible;
+            - here one Jump completes and update the rightAirport,leftAirport and flightCount.
+            - Again while loop will run if we have not reached the last airport;
+        c) return flightCount;
+
+    
+
+*/
